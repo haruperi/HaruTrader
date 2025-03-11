f8b4b00c9fba2e3c6bea0013f6e46547a0c14d7f
rharu <r.haruperi@hotmail.com>
2025-03-11 17:28:17 +0200

    Removed history file

--- .history/algotrader/integrations/mt5/__init___20250311170957.py
+++
@@ -1,8 +0,0 @@
-"""
-MetaTrader 5 integration module.
-
-This module provides integration with the MetaTrader 5 trading platform.
-"""
-from .connection import MT5Connection
-
-__all__ = ['MT5Connection']
